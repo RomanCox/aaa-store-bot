e456b52d42aad594090c38cbe69c8d82cd8671af
Roman Cox <rom.cox.dev@gmail.com>
2026-04-04 13:06:31 +0300

    fix working hours

--- src/utils/callbackBuilder.ts
+++ src/utils/callbackBuilder.ts
@@ -1,8 +1,6 @@
 import crypto from 'crypto';
 import { setCallbackHash } from "../services/callbackHashMap";
 
-// type CallbackPart = string | number | undefined | null;
-
 function hashString(str: string) {
 	return crypto.createHash('md5').update(str).digest('hex');
 }
@@ -13,10 +11,3 @@ export function buildCallbackData(...parts: string[]): string {
 	setCallbackHash(hash, raw);
 	return hash;
 }
-
-// export function buildCallbackData(...parts: CallbackPart[]): string {
-// 	return parts
-// 		.filter((p): p is string | number => p !== undefined && p !== null)
-// 		.map(String)
-// 		.join("::");
-// }
