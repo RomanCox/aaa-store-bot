0a9b9a64dfb7fdf2bc78c6d8183699556a76609d
RomanCox <rom.cox.dev@gmail.com>
2026-06-04 01:05:07 +0300

    fix workinghours

--- src/utils/workingHours.ts
+++ src/utils/workingHours.ts
@@ -3,15 +3,18 @@ import { COMMON_TEXTS } from "../texts";
 import { isAdmin } from "../services/users.service";
 
 export function isWorkingHours(): boolean {
-  // const now = new Date();
+  const now = new Date();
 
-  // const minskTime = new Date(
-  //   now.toLocaleString("en-US", { timeZone: "Europe/Minsk" })
-  // );
+  const minskTime = new Date(
+    now.toLocaleString("en-US", { timeZone: "Europe/Minsk" })
+  );
 
-  // const hours = minskTime.getHours();
+  const hours = minskTime.getHours();
 
-  const hours = new Date().getHours();
+  console.log(`[DEBUG] hours = ${hours}, DEV = ${process.env.DEV}`);
+
+  const working = (hours >= 11 && hours < 21);
+  console.log(`[DEBUG] working = ${working}`);
 
   return process.env.DEV ? true : hours >= 11 && hours < 21;
 }
